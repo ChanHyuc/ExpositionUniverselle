57af017569c8399d872091b27746b84f5e01ca35
ChanHyuc <whk1140@gmail.com>
2024-10-08 18:56:16 +0900

    Refactor: 폴더 구조 변경

---
+++ ExpositionUniverselle/Controller/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExpositionUniverselle
+//
+//  Created by ChanHyuc on 10/1/24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+
+    private func configureUI() {
+        view.backgroundColor = .systemBackground
+        
+    }
+    
+    let encoder = JSONEncoder()
+    
+
+}
